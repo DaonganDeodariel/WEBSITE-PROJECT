aa74edbcc85a3396ade8644332fb5042ab321901
Deodariel Daongan <79815989+DaonganDeodariel@users.noreply.github.com>
2021-05-05 17:27:55 +0800

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+
+//for the navbar
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+
+//navbar ends here
+
+
+
+//start of slides images
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
+//end of slides images
+
